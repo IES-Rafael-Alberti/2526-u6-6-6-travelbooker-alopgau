8a44d5b71add944537e10e770bec201b7e784751
alopgau <alopezg@ieslabahia.es>
2026-03-12 17:30:35 +0100

    ReservaHotel quitada regex

--- src/main/kotlin/ReservaHotel.kt
+++ src/main/kotlin/ReservaHotel.kt
@@ -3,7 +3,6 @@ import dominio.Reserva
 class ReservaHotel private constructor(descripcion: String, fechaCreacion: String, ubicacion: String, numeroCoches: String) : Reserva() {
     companion object {
         fun creaInstancia(descripcion: String,fechaCreacion: String,ubicacion: String,numeroCoches: String) = ReservaHotel(descripcion,fechaCreacion,ubicacion,numeroCoches)
-        val regexHora = "^[01][0-9]:[012345][0-9]|2[0123]:[012345][0-9]$".toRegex()
     }
     override val descripcion = descripcion
     override val fechaCreacion = fechaCreacion
