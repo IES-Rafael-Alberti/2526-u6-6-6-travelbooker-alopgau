1322ddf53cf24cc2c7ed2d35c07651d89e5c2272
alopgau <alopezg@ieslabahia.es>
2026-03-16 18:02:32 +0100

    generico introducido

--- src/main/kotlin/IReservaRepositorio.kt
+++ src/main/kotlin/IReservaRepositorio.kt
@@ -1,7 +1,7 @@
 package datos
 import dominio.Reserva
-interface IReservaRepositorio {
-    val reservas : MutableMap<Int, Reserva>
+interface IReservaRepositorio<T> {
+    val reservas : MutableMap<T, Reserva>
     fun agregarReserva(reserva: Reserva): Reserva?
     fun obtenerTodas() : List<Reserva>
 
